f3941086ab3f0996acf78f8d02dcfb677701fa13
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-09-10 23:26:14 +0530

    created variables

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const main = document.querySelector(".main");
+const sidebar = document.querySelector(".sidebar");
+const addNew = document.querySelector(".add");
+
+const openModalBtn = document.querySelector(".open-modal");
+const overlayBtn = document.querySelector(".overlay");
+
+const taskName = document.querySelector(".task-name");
+const addTaskBtn = document.querySelector(".add-task-btn");
+
+
 
